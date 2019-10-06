9c42001a2eb1a503c1903ed7a3828d1bbce0a177
Miguel González Mora <gonzalezmoramiguel@gmail.com>
2019-10-06 17:55:08 +0200

    Actualización Carpeta EjerciciosPRO4

---
+++ EJERCICIOS PRO 04/Ejercicio15.java
@@ -0,0 +1,57 @@
+public class Ejercicio15 {
+  public static void main(String [] args) {
+    // Decir mes y día de nacimiento y que el programa nos lo resuelva con nuestro horóscopo.
+      
+    System.out.println("En este programa vamos a dibujar una pirámide con lo que pongamos en teclado");
+    System.out.println("Introduzca en el teclado lo que quieras que se haga pirámide: ");
+    String pira;
+    pira = System.console().readLine(); 
+    
+    System.out.println("Elija el número hacia donde quiera que vaya la pirámide (1-4): ");
+    System.out.println("1 - Dirección hacia arriba ");
+    System.out.println("2 - Diracción hacia la derecha ");
+    System.out.println("3 - Dirección hacia abajo ");
+    System.out.println("4 - Dirección hacia la izquierda ");
+    System.out.println(" ");
+    int direccion = Integer.parseInt(System.console().readLine());
+    System.out.println(" ");
+   
+    
+    switch(direccion) {
+      case 1:
+        System.out.println("    " + pira + pira + "    ");
+        System.out.println("   " + pira + pira + pira + pira + "   ");
+        System.out.println("  " + pira + pira + pira + pira + pira + pira + "  ");
+        System.out.println(" " + pira + pira + pira + pira + pira + pira + pira + pira +" ");
+        System.out.println(pira + pira + pira + pira + pira + pira + pira + pira + pira + pira);
+        
+        break;
+      case 2:
+        System.out.println(pira);
+        System.out.println(pira + " " + pira);
+        System.out.println(pira + " " + pira + " " + pira);
+        System.out.println(pira + " " + pira);
+        System.out.println(pira); 
+        
+        break;
+      case 3:
+        System.out.println(pira + pira + pira + pira + pira + pira + pira + pira + pira + pira);
+        System.out.println(" " + pira + pira + pira + pira + pira + pira + pira + pira +" ");
+        System.out.println("  " + pira + pira + pira + pira + pira + pira + "  ");
+        System.out.println("   " + pira + pira + pira + pira + "   ");
+        System.out.println("    " + pira + pira + "    ");
+        break;
+      case 4:
+        System.out.println("    " + pira);
+        System.out.println("  " + pira + " " + pira);
+        System.out.println(pira + " " + pira + " " + pira);
+        System.out.println("  " + pira + " " + pira);
+        System.out.println("    " + pira); 
+      default:
+      System.out.println("No ha seleccionado una de las opciones (1-4)");
+    }
+    
+
+ }   
+}
+
---
+++ EJERCICIOS PRO 04/Ejercicio16.java
@@ -0,0 +1,90 @@
+public class Ejercicio16 {
+  public static void main(String [] args) {
+    // 
+      
+    System.out.println("En este programa vamos a comprobar la fidelidad con tu pareja");
+    System.out.println("A continuación se realizarán una serie de situaciones las cuales tienes");
+    System.out.println("que responder con 'v' si es verdadero y con 'f' en el caso de que");
+    System.out.println("la situación sea falsa");
+    
+    String respuesta; 
+    int puntos = 0;
+    
+    System.out.println("1. Tu pareja parece estar más inquieta de lo normal sin ningún motivo aparente.");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+    
+    System.out.println("2. Ha aumentado sus gastos de vestuario.");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+    
+    System.out.println("3. Ha perdido el interés que mostraba anteriormente por ti.");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+      
+    System.out.println("4. Ahora se afeita y se asea con más frecuencia (si es hombre) o ahora se arregla el pelo y se asea con más frecuencia (si es mujer).");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+      
+    System.out.println("5. No te deja que mires la agenda de su teléfono móvil.");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+      
+    System.out.println("6. A veces tiene llamadas que dice no querer contestar cuando estás tú delante.");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+      
+    System.out.println("7. Últimamente se preocupa más en cuidar la línea y/o estar bronceado/a.");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+      
+    System.out.println("8. Muchos días viene tarde después de trabajar porque dice tener mucho más trabajo.");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+      
+    System.out.println("9. Has notado que últimamente se perfuma más.");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+      
+    System.out.println("10. Se confunde y te dice que ha estado en sitios donde no ha ido contigo.");
+    respuesta = System.console().readLine();
+      if (respuesta.equals("v")) {
+      puntos += 3;
+      }
+      
+    System.out.println("El resultado del test es...");
+    
+    if ( puntos <= 10 ) {
+      System.out.println("¡Enhorabuena! tu pareja parece ser totalmente fiel.");
+    }
+    
+    if ( (puntos > 11 ) && (puntos <= 22) ) {
+      System.out.println("Quizás exista el peligro de otra persona en su vida o en su mente, aunque seguramente será algo sin importancia. No bajes la guardia.");    
+    }
+    
+    if ( puntos >= 22 ) {
+      System.out.println("Tu pareja tiene todos los indicios para estar viviendo un romance con otra persona. Te aconsejamos que indagues un poco más y averigües qué es lo que está pasando por su cabeza.");
+    }    
+    
+
+ }   
+}
+
