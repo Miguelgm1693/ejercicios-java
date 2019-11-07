e12d1b55187b57b6247e6ff27e1937253623ae17
Miguelgm1693 <gonzalezmoramiguel@gmail.com>
2019-11-07 09:34:24 +0100

    Ejercicios Tema 7

---
+++ EJERCICIOS PRO 07/Ejercicio01.java
@@ -0,0 +1,21 @@
+package tema7;
+
+public class Ejercicio01 {
+
+  public static void main(String[] args) {
+    int[] num;
+    num = new int [12];
+    
+    num [0] = 39;
+    num [1] = -2;
+    num [4] = 0;
+    num [6] = 14;
+    num [8] = 5;
+    num [9] = 120;
+    
+    for (int i = 0; i < 12; i++){
+      System.out.printf("num[%2d]: %d\n", i, num[i]);
+    }
+    
+  }
+}
