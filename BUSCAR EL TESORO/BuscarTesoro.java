7505ba7f72e129f233542c8fdc7ebf118452add8
Miguelgm1693 <gonzalezmoramiguel@gmail.com>
2019-11-21 08:22:40 +0100

    Juego de buscar el tesoro

--- BUSCAR EL TESORO/BuscarTesoro.java
+++ BUSCAR EL TESORO/BuscarTesoro.java
@@ -1,45 +1,108 @@
 package buscartesoro;
 
+import java.util.Scanner;
+
 public class BuscarTesoro {
 
   public static void main(String[] args) {
-    //constantes
+    Scanner s = new Scanner(System.in);
+    //constantes contenido de las casillas se pone final y en mayuscula
     final int VACIO = 0;
     final int MINA = 1;
     final int TESORO = 2;
     final int INTENTO = 3;
-    
+
     int x;
     int y;
-    int [][] matriz = new int [10][8];
-    
-    //mina
-    int minaX = (int) Math.random() * 10;
-    int minaY = (int) Math.random() * 8;
-    matriz [minaX][minaY] = MINA;
-    
-    //tesoro
+    int[][] matriz = new int[10][8];
+
+    //iniciar el array
+    for (x = 0; x < 9; x++) {
+      for (y = 0; y < 7; y++) {
+        matriz[x][y] = VACIO;
+      }
+    }
+
+    //posición mina
+    int minaX = (int) (Math.random() * 10);
+    int minaY = (int) (Math.random() * 8);
+    matriz[minaX][minaY] = MINA;
+
+    //posición tesoro
     int tesoroX;
     int tesoroY;
-    do {      
-      tesoroX = (int) Math.random() * 10;
-      tesoroY = (int) Math.random() * 8;
-    } while (minaX == tesoroX && minaY == tesoroY);
-    
-    matriz [tesoroX][tesoroY] = TESORO;
-    
-    System.out.println("ENCUENTRA EL TESORO");
-    
-    //imprimir matriz
-    for (y = 8; y > 0; y--) {
-      System.out.print(y + "|");
-      
-      for (int x = 0; x < 10 ; x++) {
-      
+    do {
+      tesoroX = 0;//(int) (Math.random() * 10);
+      tesoroY = 1;//(int) (Math.random() * 8);
+    } while ((minaX == tesoroX) && (minaY == tesoroY));
+
+    matriz[tesoroX][tesoroY] = TESORO;
+
+    System.out.println("¡ENCUENTRA EL TESORO!");
+    boolean salir = false;
+    String carac = "";
+
+    do {
+      //imprimir matriz
+      for (y = 7; y > 0; y--) {
+        System.out.print(y + "|");
+
+        for (x = 0; x < 10; x++) {
+          if (matriz[x][y] == INTENTO) {
+            System.out.print("X ");
+          } else {
+            System.out.print("  ");
+          }
+        }
+        System.out.println();
       }
-    
+      System.out.println(" --------------------\n  0 1 2 3 4 5 6 7 8 9 \n");
+
+      //pedir la coordenada que desea buscar el jugador
+      System.out.println("Pon una coordenada X: ");
+      x = Integer.parseInt(s.nextLine());
+      System.out.println("Pon la coordenada Y: ");
+      y = Integer.parseInt(s.nextLine());
+
+      //comprobar que hay en las coordenadas indicadas por el jugador
+      switch (matriz[x][y]) {
+        case VACIO:
+          matriz[x][y] = INTENTO;
+          break;
+        case MINA:
+          System.out.println("Has explotado la mina, juego terminado.");
+          salir = true;
+          break;
+        case TESORO:
+          System.out.println("¡Enhorabuena, has conseguido el tesoro!");
+          salir = true;
+          matriz[x][y] = TESORO;
+          break;
+        default:
+      }
+    } while (!salir);
+
+    //imprimir cuadrante
+    for (y = 7; y > 0; y--) {
+      System.out.print(y + "|");
+      for (x = 0; x < 10; x++) {
+        switch(matriz[x][y]) {
+          case VACIO:
+            carac = "  ";
+            break;
+          case INTENTO:
+            carac = "X ";
+            break;
+          case TESORO:
+            carac = "# ";
+            break;
+          default:
+        }
+        System.out.print(carac);
+      }
+      System.out.println();
     }
-      
+    System.out.println(" --------------------\n   0 1 2 3 4 5 6 7 8 9 \n ");
+    
   }
-  
 }
