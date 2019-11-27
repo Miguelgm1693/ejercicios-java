7d97887b089413a50dc4379c481c90619ca0983e
Miguelgm1693 <gonzalezmoramiguel@gmail.com>
2019-11-27 09:04:42 +0100

    Actualización Tema 7

---
+++ EJERCICIOS PRO 07/Ejercicio08.java
@@ -0,0 +1,12 @@
+package tema7;
+
+import java.util.Scanner;
+
+public class Ejercicio08 {
+  public static void main(String[] args) {
+    System.out.println("Programa al que le diremos la temperatura y hará una gráfica");
+    Scanner sc = new Scanner (System.in);
+    
+  }
+  
+}
